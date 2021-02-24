d4f614ddb1b279fe9062104ed0cc754eaf31f525
Willy-tec <alown666@gmail.com>
2021-02-24 06:59:19 +0100

    réalisation de la modale lightbox

--- assets/script/galery.js
+++ assets/script/galery.js
@@ -1,6 +1,8 @@
 let data = {
     isDefine : false,
-    array : null
+    array : null,
+    galleryArray : [],
+    position : 0
 
 };
 let requestURL = "../script/FishEyeDataFR.json";
@@ -9,7 +11,6 @@ let artisteId = document.getElementsByTagName("body")[0].getAttribute("data-id")
 fetch(requestURL).then(function (reponse) {
         return reponse.json()
     }).then(function (jsonData) {
-        console.log(jsonData);
         data.isDefine= true;
         data.array = jsonData;
         fillGallery(data.array.media)
@@ -23,9 +24,6 @@ fetch(requestURL).then(function (reponse) {
 
 let orderBox = document.getElementsByClassName("order_select")[0];
 
-console.log(orderBox)
-//let rect = orderBox.parentElement.offsetTop
-//console.log("h: "+rect)
 
 const makeListBoxNode = function () {
     let node = document.createElement("div");
@@ -70,40 +68,50 @@ window.onresize = function (evt) {
 
 let galleryNode = document.getElementsByClassName("gallery")[0];
 
-const fillGallery = function (data) {
-    let galleryArray = [];
+
+const fillGallery = function (dataToFill) {
+    data.galleryArray = [];
     galleryNode.innerHTML="";
-    data.forEach(element => {
+    dataToFill.forEach(element => {
         if (element.photographerId == artisteId) {
             // console.log(element)
-            galleryArray.push(element)
+            data.galleryArray.push(element)
         }
     });
 
-   // console.log(galleryArray);
-    console.log(OrderBy(order, galleryArray));
+    OrderBy(order, data.galleryArray);
 
-    galleryArray.forEach(elt => {
+    data.galleryArray.forEach(elt => {
         galleryNode.appendChild(fabricNode(elt))
     })
 }
 
-const makeImgNode = function ( path) {
+const makeImgNode = function ( path, id) {
     let imgNode = document.createElement("img");
+
     imgNode.src = "../image/Mimi/" + path;
-    imgNode.style.width = "100%";
+   // imgNode.style.width = "100%";
   //  imgNode.style.height = "100%";
     imgNode.alt = makeAltText("Image", path);
     imgNode.className = "gallery_frame_element";
+    imgNode.setAttribute("data-id", id);
     return imgNode;
 }
-
-const makeVidNode = function (path) {
+const findPosition = function(evt){
+    let ind = 0;
+    let array = data.galleryArray;
+    let indArray = array.map(elt => elt.id)
+    data.position = indArray.indexOf(parseInt(evt.target.getAttribute("data-id"),10))
+    openLightBox()
+    
+}
+const makeVidNode = function (path, id) {
     let vidNode = document.createElement("video");
     let srcNode = document.createElement("source");
     srcNode.src = "../image/Mimi/" + path;
     srcNode.setAttribute("type", "video/mp4");
     vidNode.style.width = "100%";
+    vidNode.setAttribute("data-id", id);
 //    vidNode.setAttribute("controls", "");
     vidNode.appendChild(srcNode);
     vidNode.className = "gallery_frame_element";
@@ -114,9 +122,15 @@ const makeVidNode = function (path) {
 const fabricNode = function (media) {
     let mediaNode = document.createElement("div");
     mediaNode.className= "gallery_frame";
-    if (media.image) mediaNode.appendChild(makeImgNode(media.image))
-    else if (media.video) mediaNode.appendChild(makeVidNode(media.video))
+    let clickableNode = document.createElement("a")
+    if (media.image) clickableNode.appendChild(makeImgNode(media.image, media.id))
+    else if (media.video) clickableNode.appendChild(makeVidNode(media.video, media.id))
 
+    
+    clickableNode.href="javascript:void(0)";
+    clickableNode.onclick = findPosition;
+
+    mediaNode.appendChild(clickableNode)
     mediaNode.appendChild(makeDescriptionNode(media))
 
     return mediaNode;
@@ -175,4 +189,92 @@ const makeDescriptionNode = function(media){
     descriptionNode.appendChild(likeNode);
     descriptionNode.className = "gallery_frame_element_description"
     return descriptionNode;
+}
+
+const openLightBox = function(){
+
+    let lightBox = document.getElementById("lightbox");
+    setImgLightBox();
+    lightBox.style.display= "flex";
+    
+    let crossButton = lightBox.querySelector(".lightbox_frame_right_cross");
+    let leftButton = lightBox.querySelector(".lightbox_frame_left_arrow");
+    let rightButton = lightBox.querySelector(".lightbox_frame_right_arrow");
+
+    leftButton.focus();
+    leftButton.blur();
+    
+    leftButton.addEventListener("click", clickLeftLightBox);
+    document.addEventListener("keyup", keyListener);
+    rightButton.addEventListener("click", clickRightLightBox);
+    crossButton.addEventListener("click", closeEventListener)
+}
+const closeEventListener = function(){
+    let lightBox = document.getElementById("lightbox");
+    let crossButton = lightBox.querySelector(".lightbox_frame_right_cross");
+    let leftButton = lightBox.querySelector(".lightbox_frame_left_arrow");
+    let rightButton = lightBox.querySelector(".lightbox_frame_right_arrow");
+
+    lightBox.style.display= "none";
+    crossButton.removeEventListener("click", this);
+    rightButton.removeEventListener("click", clickRightLightBox);
+    leftButton.removeEventListener("click", clickLeftLightBox);
+    document.removeEventListener("keyup", keyListener)
+}
+const setImgLightBox = function(){
+    let lightBox = document.getElementById("lightbox");
+    let nodeLightBox = lightBox.querySelector(".lightbox_frame_principal");
+    nodeLightBox.innerHTML= "";
+  //  if(data.galleryArray[data.position].image)imgLightBox.src = "../image/Mimi/"+data.galleryArray[data.position].image;
+    nodeLightBox.appendChild(fabricLightBoxNode(data.galleryArray[data.position]))
+    nodeLightBox.appendChild(fabricLightBoxTitle(data.galleryArray[data.position].image||data.galleryArray[data.position].video))
+}
+ const keyListener = function(e){
+    e.preventDefault();
+    switch(e.key){
+        case "ArrowLeft" : clickLeftLightBox(); break;
+        case "ArrowRight": clickRightLightBox(); break;
+        case "Escape" : closeEventListener(); break;
+        default : break;
+    }
+
+} 
+const clickLeftLightBox = function(){
+    let maxVal = data.galleryArray.length-1;
+    if(data.position==0) data.position = maxVal;
+    else data.position -= 1;
+    document.querySelector(".lightbox_frame_left_arrow").focus()
+    setImgLightBox();
+}
+const clickRightLightBox = function(){
+    let maxVal = data.galleryArray.length-1;
+    if(data.position==maxVal) data.position = 0;
+    else data.position += 1;
+    document.querySelector(".lightbox_frame_right_arrow").focus()
+    setImgLightBox();
+}
+
+const fabricLightBoxNode = function(elt){
+    let frame = document.querySelector(".lightbox_frame_principal");
+    let mediaNode = null;
+    let str = findTitle(elt.image||elt.video)
+    if(elt.image){
+        mediaNode = document.createElement("img");
+        mediaNode.src = "../image/Mimi/"+elt.image;
+        mediaNode.className = "lightbox_frame_principal_media";
+        mediaNode.alt = "photo de " + str.title;
+    }
+    else if(elt.video){
+        mediaNode = document.createElement("video");
+        mediaNode.src = "../image/Mimi/"+elt.video;
+        mediaNode.setAttribute("controls","");
+        mediaNode.className = "lightbox_frame_principal_media";
+    };
+    return mediaNode;
+}
+const fabricLightBoxTitle = function(str){
+    let paragraphe= document.createElement("p");
+    paragraphe.className="lightbox_frame_principal_title";
+    paragraphe.textContent = findTitle(str).title;
+    return paragraphe;
 }
